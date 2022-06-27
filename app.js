643c1764111663ed488b2146da52fcd510bf4a6b
Simeon Spasov <47272153+PotatoKingBG@users.noreply.github.com>
2022-06-27 15:37:07 +0300

    commit

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
 const menu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.navbar__menu')
 const navLogo = document.querySelector('#navbar__logo')
+const img1 = document.querySelector('#img1')
+const img2 = document.querySelector('#img2')
+const img3 = document.querySelector('#img3')
+const img4 = document.querySelector('#img4')
 
 // display mobile menu
 const mobileMenu = () => {
     menu.classList.toggle('is-active')
     menuLinks.classList.toggle('active')
 }
-
+const Expand = () => {
+    img1.classList.toggle('is-active')
+}
+const Expand2 = () => {
+    img2.classList.toggle('is-active')
+}
+const Expand3 = () => {
+    img3.classList.toggle('is-active')
+}
+const Expand4 = () => {
+    img4.classList.toggle('is-active')
+}
 menu.addEventListener('click', mobileMenu);
-
+img2.addEventListener('click', Expand2);
+img3.addEventListener('click', Expand3);
+img1.addEventListener('click', Expand);
+img4.addEventListener('click', Expand4);
 // Active menu while scrolling
 const highlightMenu = () => {
     const elem = document.querySelector('.highlight')
